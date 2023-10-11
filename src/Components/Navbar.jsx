1d734e38ca3f2a294ab95a506d3a020dbf0f9bde
Felipe Fernandez H <felipefelizx@gmail.com>
2023-10-11 01:34:14 -0300

    Test - Test vercel

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import "./../Styles/Navbar.css"
+//import "./../Styles/Navbar.css"
 
 function NavBar() {
   return (
