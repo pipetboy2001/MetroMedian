e3d9d5601678e68fa35e058af13d55ef5ffa3375
Felipe Fernandez H <felipefelizx@gmail.com>
2023-10-24 03:12:53 -0300

    Feature - Modal mapa

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import Logo from "../assets/Logo.svg";
+import Mapa from "../assets/mapa-metro-santiago.jpg";
 
 function NavBar() {
+  const [showMapModal, setShowMapModal] = useState(false);
+
+  const handleClose = () => setShowMapModal(false);
+  const handleShow = () => setShowMapModal(true);
+
   return (
     <>
       <Navbar className="bg-body-tertiary justify-content-center">
-        <Container>
-          <Navbar.Brand className="d-flex align-items-center">
-            <span className="titulo-navbar mr-2">METRO </span>
+        <Container className="d-flex align-items-center justify-content-center text-center">
+          <Navbar.Brand className="">
+            <span className="titulo-navbar">METRO</span>
             <img
-              alt=""
+              title="Ver mapa del metro"
+              alt="Metro-Median"
               src={Logo}
               width="50"
               height="40"
-              className="d-inline-block align-top"
+              className="d-inline-block align-top mx-2"
+              onClick={handleShow}
+              style={{ cursor: "pointer" }}
             />
-            <span className="titulo-navbar ml-2"> MEDIAN</span>
+            <span className="titulo-navbar">MEDIAN</span>
           </Navbar.Brand>
         </Container>
       </Navbar>
+
+      <Modal
+        show={showMapModal}
+        onHide={handleClose}
+        centered // Esta línea centra el modal en la pantalla
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Mapa del Metro</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            src={Mapa}
+            alt="Mapa del Metro Santiago"
+            style={{ width: "100%" }}
+          />
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
